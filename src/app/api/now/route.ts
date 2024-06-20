b10f9875eea09a8393396948b3f8a7ad0da1898a
Anthony Cueva <hi.cuevantn@gmail.com>
2024-06-20 13:46:29 -0500

    feat: best prices

--- src/app/api/now/route.ts
+++ src/app/api/now/route.ts
@@ -1,4 +1,5 @@
 export const runtime = "edge";
+export const revalidate = 15;
 
 export async function GET() {
   const url = "https://cuantoestaeldolar.pe";
@@ -13,7 +14,6 @@ export async function GET() {
       "sec-fetch-mode": "cors",
       "sec-fetch-site": "same-origin",
     },
-    next: { revalidate: 60 },
   };
 
   try {
@@ -21,8 +21,7 @@ export async function GET() {
 
     const page = await response.text();
 
-    // inside         <script id="__NEXT_DATA__" type="application/json">
-
+    // inside <script id="__NEXT_DATA__" type="application/json">
     const match = page.match(
       /<script id="__NEXT_DATA__" type="application\/json">(.+?)<\/script>/
     );
@@ -35,7 +34,6 @@ export async function GET() {
     }
 
     const data = JSON.parse(match[1]);
-    console.log(data.props.pageProps.onlineExchangeHouses);
 
     const er: Array<{ entity: string; buy: number; sell: number }> =
       data.props.pageProps.onlineExchangeHouses.map((ex: any) => ({
@@ -47,14 +45,21 @@ export async function GET() {
     const sellAvg = er.reduce((acc, curr) => acc + curr.sell, 0) / er.length;
     const buyAvg = er.reduce((acc, curr) => acc + curr.buy, 0) / er.length;
 
+    const minSell = Math.min(...er.map((e) => e.sell));
+    const maxBuy = Math.max(...er.map((e) => e.buy));
+
     return Response.json({
       updatedAt: new Date().toISOString(),
+      best: {
+        maxBuy: er.find((e) => e.buy === maxBuy),
+        minSell: er.find((e) => e.sell === minSell),
+      },
       meta: {
-        sellAvg: Math.round(sellAvg * 1000) / 1000,
         buyAvg: Math.round(buyAvg * 1000) / 1000,
+        sellAvg: Math.round(sellAvg * 1000) / 1000,
         count: er.length,
       },
-      data: er,
+      houses: er,
     });
   } catch (error) {
     return Response.json({ error: "error" }, { status: 500 });
